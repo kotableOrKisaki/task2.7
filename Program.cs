a02a04b54ce7a60e7db0dc8a398301447ca8bf87
kotableOrKisaki <super_misha05@mail.ru>
2022-08-29 19:42:36 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            char symbol;
+            int lenghtSymbolString;
+            int borderSymbol = 2;
+
+            Console.Write("Твое имя - \t");
+            name = Console.ReadLine();
+            Console.Write("Введи любой символ - \t");
+            symbol = Convert.ToChar(Console.ReadLine());
+            lenghtSymbolString = name.Length+borderSymbol;
+
+            for (int i=0; i< lenghtSymbolString; i++)
+            {
+                Console.Write(symbol);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(symbol+name+symbol);
+
+            for (int i = 0; i < lenghtSymbolString; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+    }
+}
